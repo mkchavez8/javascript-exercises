3370126d63e90cbaf61612942ed5dc79b6556375
Mossimo Chavez <114546057+mkchavez8@users.noreply.github.com>
2022-12-12 15:21:40 -0500

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+   let string = '';
+    if (number < 0) {
+        return 'ERROR';
+    }
+    for (i = 0; i < number; i++) {
+        string += word;
+    }
+   return string;
 };
 
 // Do not edit below this line
