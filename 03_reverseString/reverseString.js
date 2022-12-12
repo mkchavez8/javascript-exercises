a8d2e8218eb74866b9891b34e44731e5ca4f28c7
Mossimo Chavez <114546057+mkchavez8@users.noreply.github.com>
2022-12-12 15:50:56 -0500

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    const reversedArray = array.reverse();
+    const reversedString = reversedArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
