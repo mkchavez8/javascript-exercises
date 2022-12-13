4844c4e02a5d9a29a415b0995c1875b2ef00c324
Mossimo Chavez <114546057+mkchavez8@users.noreply.github.com>
2022-12-13 16:18:45 -0500

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof(num1 && num2) !== 'number' || ((num1 || num2) < 0)) {
+        return "ERROR";
+    }
+    else if (num1 < num2) {
+        for (i = 0; i < num2; i++) {
+            sum += (i + 1);
+        }
+    return sum;
+    }
+    else if (num1 > num2) {
+        for (i = 0; i < num1; i++) {
+            sum += (i + 1);
+        }
+    return sum;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
