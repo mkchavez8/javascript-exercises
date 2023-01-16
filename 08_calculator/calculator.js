4fb00e8688c890fd56b7117014f0f1b085aeea36
Mossimo Chavez <114546057+mkchavez8@users.noreply.github.com>
2023-01-16 12:49:06 -0500

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const sum = function(array) {
+  return array.reduce((total, item) => total + item, 0);
 };
 
-const subtract = function() {
-	
+const multiply = function(array) {
+  return array.reduce((total, item) => total * item, 1);
 };
 
-const sum = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const multiply = function() {
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let total = 1
+	for (i = num; i > 0; i--) {
+    total *= i;
+  };
+  return total;
 };
 
 // Do not edit below this line
