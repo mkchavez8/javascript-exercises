2e43d586b8a0ebbf8867bbaf4442176f4a252923
Mossimo Chavez <114546057+mkchavez8@users.noreply.github.com>
2023-01-17 16:32:41 -0500

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let firstNumber = 0
+    let secondNumber = 1;
+    if (number < 0) {
+        return 'OOPS';
+    }
+    for (i = 1; i < number; i++) {
+        let nextNumber = secondNumber;
+        secondNumber = firstNumber + secondNumber;
+        firstNumber = nextNumber;
+    };
+    return secondNumber;
 };
 
 // Do not edit below this line
